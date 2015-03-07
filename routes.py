ea042dd2e1c3b45c6d3a54bfbdb6935bb978bd87
revant117 <revantjha.94@gmail.com>
2015-03-07 06:30:07 +0530

    initial

---
+++ routes.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template,request,url_for
+import subprocess
+from flask import send_file
+
+
+import os
+
+app = Flask(__name__,static_folder='static', static_url_path='')
+
+
+@app.route('/')
+def home():
+  return render_template('part9.html')
+
+
+
+
+
+
+port = int(os.environ.get('PORT', 3000))
+
+if __name__ == '__main__':
+     app.run(debug=True,port=port)
